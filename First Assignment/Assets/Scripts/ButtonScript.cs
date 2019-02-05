49689f3119c9d24bdbcee2bb46b22807fbfd5d62
tobbe <tobbejj@hotmail.com>
2019-02-05 11:09:18 +0100

    first commit

---
+++ First Assignment/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public string _printText;
+    public GameObject cube;
+    public int amount;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void printText()
+    {
+        print(_printText);
+        
+    }
+
+    public void playSound()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    public void instantiateCube()
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            Instantiate(cube, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void soundSpawn()
+    {
+        GetComponent<AudioSource>().Play();
+        for (int i = 0; i < amount; i++)
+        {
+            Instantiate(cube, transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ First Assignment/Assets/Scripts/Movement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float jump = Input.GetAxis("Jump");
+
+        //0.0f
+        Vector3 movement = new Vector3(moveHorizontal, jump, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if(this.gameObject.transform.position.y < 0)
+        {
+            print("Player Respawned");
+            this.gameObject.transform.position = new Vector3(1, 1, 0);
+        }
+        
+    }
+}
+
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.z--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.z++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+    }
+}
+*/
